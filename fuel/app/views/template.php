2a7bf4e0b18a59a434a4b967bbe810e345b0441c
尾々野聖 <keks564976@gmail.com>
2018-12-16 16:55:25 +0900

    Create template.php & backup pre_template

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -1,45 +1,70 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>
-	<?php echo Asset::css('bootstrap.css'); ?>
-	<style>
-		body { margin: 40px; }
-	</style>
+  <style type="text/css">
+    #wrapper {
+      margin:0 auto;
+      width:960px;
+    }
+		#wrapper > h1 {
+			background: aqua;
+		}
+    #middle {
+      background: #fcc;
+    }
+
+    #menu {
+      float:left;
+      width:230px;
+    }
+    #content {
+      float:left;
+      padding-left: 20px;
+      background:#fff;
+      width:710px;
+    }
+    #footer {
+      background:#cfc;
+      padding:10px;
+    }
+    .clearfix:after {
+      content:"";
+      display: block;
+      clear:both;
+      height: 0;
+      visibility: hidden;
+    }
+    .clearfix {
+      min-height: 1px;
+    }
+    * html .clearfix {
+      height: 1px;
+      /*¥*//*/
+      height: auto;
+      overflow:hidden;
+      /**/
+    }
+  </style>
+  <meta charset="utf-8">
+  <title><?php echo $title; ?></title>
 </head>
 <body>
-	<div class="container">
-		<div class="col-md-12">
-			<h1><?php echo $title; ?></h1>
-			<hr>
-<?php if (Session::get_flash('success')): ?>
-			<div class="alert alert-success">
-				<strong>Success</strong>
-				<p>
-				<?php echo implode('</p><p>', e((array) Session::get_flash('success'))); ?>
-				</p>
-			</div>
-<?php endif; ?>
-<?php if (Session::get_flash('error')): ?>
-			<div class="alert alert-danger">
-				<strong>Error</strong>
-				<p>
-				<?php echo implode('</p><p>', e((array) Session::get_flash('error'))); ?>
-				</p>
-			</div>
-<?php endif; ?>
-		</div>
-		<div class="col-md-12">
-<?php echo $content; ?>
-		</div>
-		<footer>
-			<p class="pull-right">Page rendered in {exec_time}s using {mem_usage}mb of memory.</p>
-			<p>
-				<a href="http://fuelphp.com">FuelPHP</a> is released under the MIT license.<br>
-				<small>Version: <?php echo e(Fuel::VERSION); ?></small>
-			</p>
-		</footer>
-	</div>
+  <div id="wrapper">
+    <h1><?php echo $title; ?></h1>
+  </div>
+  <div id="middle" class="clearfix">
+    <div id="menu">
+      <ul>
+        <li>FuelPHP</li>
+        <li>Controllerとは</li>
+        <li>Viewとは</li>
+        <li>Modelとは</li>
+      </ul>
+    </div>
+    <div id="content">
+      <?php echo $content; ?>
+    </div>
+  </div>
+  <div id="footer">&copy; Satoshi Ono 2018</div>
 </body>
 </html>
