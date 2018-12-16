1e485e8fd37f212d5b5840a231df5700af1f9bd6
尾々野聖 <keks564976@gmail.com>
2018-12-16 16:54:02 +0900

    Rewrite db.php

--- fuel/app/config/development/db.php
+++ fuel/app/config/development/db.php
@@ -6,7 +6,7 @@
 return array(
 	'default' => array(
 		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=todo',
+			'dsn'        => 'mysql:host=127.0.0.1;dbname=todo',
 			'username'   => 'root',
 			'password'   => '',
 		),
